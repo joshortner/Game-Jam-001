01886a38b8e691dec08e273cf8ba4d65b63cd571
Josh <jlortner@gmail.com>
2023-05-18 17:14:45 -0600

    super loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,31 @@
 #include "bullet_killer.h"
 
 #include "SFML/Window.hpp"
+#include "SFML/Audio.hpp"
+#include "SFML/Graphics.hpp"
 
 #include <stdio.h>
 
 int main() {
     printf("TEST: %s\n", bk::get_asset_dir());
 
-    // Request a 24-bits depth buffer when creating the window
-    sf::ContextSettings contextSettings;
-    contextSettings.depthBits = 24;
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML window");
 
-    // Create the main window
-    sf::Window window(sf::VideoMode({ 640, 480 }), "SFML window with OpenGL", sf::Style::Default, contextSettings);
+    // Start the game loop
+    while (window.isOpen()) {
+        // Process events
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            // Close window: exit
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        // Clear screen
+        window.clear();
+        
+        // Update the window
+        window.display();
+    }
+   
+    return EXIT_SUCCESS;
 }
